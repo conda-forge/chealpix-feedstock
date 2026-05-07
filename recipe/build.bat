715b3fe01a896518a0485f4b6426b45cc15937b3
Yuan Feng <feng1m8@outlook.com>
2026-05-07 21:08:50 +0800

    fix: add source files and windows tests

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,3 +1,11 @@
+cd "%SRC_DIR%\src\C\autotools"
+del /f /q chealpix.c chealpix.h test_chealpix.c
+copy /y "..\subs\chealpix.c" .
+copy /y "..\subs\chealpix.h" .
+copy /y "..\subs\test_chealpix.c" .
+if %ERRORLEVEL% neq 0 exit 1
+
+cd "%SRC_DIR%"
 call %BUILD_PREFIX%\Library\bin\run_autotools_clang_conda_build.bat
 if %ERRORLEVEL% neq 0 exit 1
 
